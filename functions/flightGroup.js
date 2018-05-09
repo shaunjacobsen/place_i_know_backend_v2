f9a19806fa91355c3a1e6dbb22c7ac601f105b07
Shaun Jacobsen <shaun.d.jacobsen@gmail.com>
2018-05-08 23:12:36 -0500

    Refactoring

--- functions/flightGroup.js
+++ functions/flightGroup.js
@@ -5,10 +5,10 @@ module.exports = {
       let flightList = [];
       let newObj = { ...group.dataValues };
       if (group.confirmed) {
-        const confirmedFlight = group.flights.filter(flight => {
+        const confirmedBooking = group.flights.filter(flight => {
           return flight.status.toLowerCase() === 'confirmed';
         })[0];
-        flightList = [confirmedFlight.flight_id];
+        flightList = [confirmedBooking.flight_id];
       } else {
         flightList = group.flights.map(flight => {
           return flight.flight_id;
