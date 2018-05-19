51de265784757ca7322036334e804ca942f4f805
Shaun Jacobsen <shaun.d.jacobsen@gmail.com>
2018-05-19 07:51:48 -0500

    Forgot to require morgan library

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const compress = require('compression');
 const helmet = require('helmet');
+const morgan = require('morgan');
 require('./config/config');
 
 const app = express();
