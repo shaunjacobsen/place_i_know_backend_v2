954b6a69e79785f48e8bee239fb3c221b4e76065
Shaun Jacobsen <shaun.d.jacobsen@gmail.com>
2018-05-23 09:23:14 -0500

    Moved email microservice to app

---
+++ services/email/email.js
@@ -0,0 +1,30 @@
+const amqp = require('amqplib/callback_api');
+const { sendForgotPasswordEmail } = require('./actions/email');
+require('./../../config/config');
+
+amqp.connect(process.env.AMQP_SERVER, (err, connection) => {
+  connection.createChannel((err, channel) => {
+    const queue = 'emails';
+
+    channel.assertQueue(queue, { durable: true });
+
+    channel.consume(
+      queue,
+      message => {
+        const payload = JSON.parse(message.content.toString());
+        console.log('message received', payload);
+        switch (payload.template) {
+          case 'FORGOT_PASSWORD':
+            sendForgotPasswordEmail(payload)
+              .then(() => channel.ack(message))
+              .catch(e => console.log(e));
+            break;
+          default:
+            return;
+        }
+        console.log('message processed', message.content.toString());
+      },
+      { noAck: false }
+    );
+  });
+});
